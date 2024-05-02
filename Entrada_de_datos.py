6ff672177523f31b199df06407e3a7d5733c0fdb
Diego692024 <168148627+Diego692024@users.noreply.github.com>
2024-05-02 16:16:18 -0400

    Add files via upload
    en casa

---
+++ Entrada_de_datos.py
@@ -0,0 +1,11 @@
+nombre = ("Diego:\t ")
+edad = ("30:\t ")
+hobbie = ("Guitarra:\t ")
+
+print (f'Hola {nombre}!')
+print (f'Tu edad es: {edad}.')
+print (f'Y tu pasatiempo es: {hobbie}.')
+
+Ingrese_su_nombre =""
+nombre = input("Ingrese_su_nombre: ")
+print("Hola, " + nombre + "! ¿Cómo estás?")
